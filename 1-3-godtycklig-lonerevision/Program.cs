5910f8f378bf3d1555846794fafdbb239968a407
unknown <oo222bu@NY107X06.lnu.se>
2014-09-24 14:51:59 +0200

    ändring efter redovisning.

--- 1-3-godtycklig-lonerevision/Program.cs
+++ 1-3-godtycklig-lonerevision/Program.cs
@@ -11,33 +11,31 @@ namespace _1_3_godtycklig_lonerevision
         static void Main(string[] args)
         {
             //ange antal löner som ska matas in samt anropa metoden ReadInt.
-            while (true)
+            do
             {
                 int numberSalaries;
 
+                Console.Clear();
+
                 numberSalaries = ReadInt("Ange antal löner att mata in: ");
-                while (numberSalaries < 2)
+                if (numberSalaries < 2)
                 {
                     Console.WriteLine();
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("Du måste ange minst två löner för att kunna göra en beräkning! ");
                     Console.ResetColor();
-                    numberSalaries = ReadInt("Ange antal löner att mata in: ");
                 }
-                
-                Console.WriteLine();
-                ProcessSalaries(numberSalaries);
+                else
+                {
+                    Console.WriteLine();
+                    ProcessSalaries(numberSalaries);
+                }
 
                 //Initiera ESC-knappen
                 Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("Tryck valfri tangent för ny beräkning - ESC avslutar.");
+                Console.Write("Tryck valfri tangent för ny beräkning - ESC avslutar.");
                 Console.ResetColor();
-                Console.WriteLine();
-                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-                {
-                    return;
-                }                
-            }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
                         
          
         }
@@ -69,22 +67,7 @@ namespace _1_3_godtycklig_lonerevision
             int[] salary = new int[count];
             for (int i = 0; i < count; i++)
 			{
-                while (true)
-                {
-                    try
-                    {
-                        Console.Write("Ange lön nummer {0}: ", (i + 1));
-                        salary[i] = int.Parse(Console.ReadLine());
-                        break;
-                    }
-                    catch (Exception)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Fel format! ");
-                        Console.ResetColor();
-                        
-                    }                    
-                }
+                salary[i] = ReadInt(String.Format("Ange lön nummer {0}: ", i + 1));
             }
 
             int[] copySalary = new int[count];
@@ -94,10 +77,7 @@ namespace _1_3_godtycklig_lonerevision
             Array.Sort(salary);
            
             //Räkna och skriva ut lönerna.
-            int spread = salary.Max() - salary.Min();
             int median = 0;
-            int avarage = (int)salary.Average();
-
             if (count % 2 != 0)
             {
                 median = salary[count / 2];
@@ -109,8 +89,8 @@ namespace _1_3_godtycklig_lonerevision
 
             Console.WriteLine("------------------------");
             Console.WriteLine("Medianlön:       {0:c0}",median);
-            Console.WriteLine("Medellön:        {0:c0}",avarage);
-            Console.WriteLine("Lönespridning:   {0:c0}",spread);
+            Console.WriteLine("Medellön:        {0:c0}",salary.Average());
+            Console.WriteLine("Lönespridning:   {0:c0}",salary.Max() - salary.Min());
             Console.WriteLine("------------------------");
             //Skriva ut lönerna i inmatat ordning
             for (int i = 0; i < copySalary.Length; i++)
